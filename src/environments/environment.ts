c421b96bfa61cf679c83416b1e15daba47e0a316
mahmuthanb <mahmuthanb@gmail.com>
2020-10-27 11:43:19 +0300

    gitignore for env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'REPLACE_YOUR_TOKENS',
-    authDomain: 'REPLACE_YOUR_TOKENS',
-    databaseURL: 'REPLACE_YOUR_TOKENS',
-    projectId: 'REPLACE_YOUR_TOKENS',
-    storageBucket: 'REPLACE_YOUR_TOKENS',
-    messagingSenderId: 'REPLACE_YOUR_TOKENS',
-    appId: 'REPLACE_YOUR_TOKENS',
-    measurementId: 'REPLACE_YOUR_TOKENS'
+    apiKey: 'AIzaSyDB1Ul8vuYQ0VX4FRWz3tEjC1tlsmlacBU',
+    authDomain: 'alsancak-3ce23.firebaseapp.com',
+    databaseURL: 'https://alsancak-3ce23.firebaseio.com',
+    projectId: 'alsancak-3ce23',
+    storageBucket: 'alsancak-3ce23.appspot.com',
+    messagingSenderId: '702155326484',
+    appId: '1:702155326484:web:f33294f8e4060e1e64d301',
+    measurementId: 'G-PVHF08SX5L'
   }
 };
